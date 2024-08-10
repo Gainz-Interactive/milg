dc88658000cf9f9aa5543cdf2c062da5f38c499a
Ernestas Kulik <ernestas@baltic.engineering>
2024-08-10 21:57:54 +0300

    audio: engine: Add API to get endpoint

--- src/audio/engine.hpp
+++ src/audio/engine.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "node.hpp"
+
 #include <memory>
 #include <miniaudio.h>
 
@@ -7,6 +9,7 @@ namespace milg::audio {
     void init();
     void destroy();
 
+    std::shared_ptr<Node> get_endpoint();
     ma_engine *get_engine();
     ma_node_graph *get_node_graph();
 
