c9860a56afe1d3ef43ca585cd23900d465f35e97
Ernestas Kulik <ernestas@baltic.engineering>
2024-08-16 06:18:36 +0300

    Use path objects instead of strings
    Application code currently passes both a string and a path to the asset
    store, the latter of which causes failures when building on Windows.
    file_not_found_error also benefits the same way.

--- include/milg/error.hpp
+++ include/milg/error.hpp
@@ -1,16 +1,17 @@
 #pragma once
 
+#include <filesystem>
 #include <format>
 #include <stdexcept>
 
 namespace milg {
     class file_not_found_error : public std::runtime_error {
     public:
-        explicit file_not_found_error(const std::string &path)
-            : std::runtime_error(std::format("file {} not found", path)), path(path) {
+        explicit file_not_found_error(const std::filesystem::path &path)
+            : std::runtime_error(std::format("file {} not found", path.string())), path(path) {
         }
 
     private:
-        std::string path;
+        std::filesystem::path path;
     };
 } // namespace milg
