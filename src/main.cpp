f8eef5bb3ec09e41e62c27c0e1fc430e1a74bf41
Martynas Gurskas <gurmart@gmail.com>
2024-08-17 12:37:16 +0300

    Remove RT light layer by default, remove unused Milg layer
    And set the window size to be a 16:9 aspect ratio
    
    Signed-off-by: Martynas Gurskas <gurmart@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,11 @@
-#include <SDL_mouse.h>
-#include <cstdio>
-#include <fstream>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/random.hpp>
-#include <iostream>
-#include <map>
-#include <milg.hpp>
-
 #include "application.hpp"
 #include "audio/audio.hpp"
-#include "window.hpp"
 
 using namespace milg;
-
-class Milg : public Layer {
-public:
-    void on_attach() override {
-        MILG_INFO("Starting Milg");
-    }
-
-    void on_update(float delta) override {
-    }
-
-    void on_event(Event &event) override {
-    }
-
-    void on_detach() override {
-        MILG_INFO("Shutting down Milg");
-    }
-};
-
-#include "lighting_main.cpp"
 
 class Milglication : public Application {
 public:
     Milglication(int argc, char **argv, const WindowCreateInfo &window_info) : Application(argc, argv, window_info) {
-        push_layer(new Milg());
-        push_layer(new GraphicsLayer());
         push_layer(new layers::Audio());
     }
 
@@ -51,11 +19,10 @@ int main(int argc, char **argv) {
     WindowCreateInfo window_info = {
         .title  = "Milg",
         .width  = 1600,
-        .height = 1008,
+        .height = 900,
     };
 
     Milglication app(argc, argv, window_info);
-
     app.run();
 
     return 0;
