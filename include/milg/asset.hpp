5a0d89a0d9a3d2090333ef9d3d7c193ed091baa6
Ernestas Kulik <ernestas@baltic.engineering>
2024-08-18 13:58:57 +0300

    asset_store: Implement preprocessor support
    This commit allows loading JSON files as JSON objects without having to
    parse bytes manually.

--- include/milg/asset.hpp
+++ include/milg/asset.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <any>
+#include <cassert>
 #include <filesystem>
 #include <memory>
 #include <nlohmann/json.hpp>
+#include <variant>
 
 namespace milg {
     class Asset {
@@ -11,38 +14,73 @@ namespace milg {
             DATA,
             IMAGE,
             SOUND,
-
-            INVALID = -1,
         };
+
+        enum class Preprocessor {
+            NONE,
+            JSON,
+        };
+
         Asset() = delete;
         Asset(Type type, char *data, std::size_t size);
+        Asset(Type type, std::any data);
         Asset(const Asset &) = delete;
         Asset(Asset &&)      = delete;
 
         Asset &operator=(const Asset &) = delete;
         Asset &operator=(Asset &&)      = delete;
 
-        virtual ~Asset();
+        virtual ~Asset() = default;
 
-        char       *get_data();
+        /**
+         * Get unprocessed baitų šmotas.
+         * Will be null if the data has been preprocessed.
+         */
+        char *get_data();
+        /**
+         * Get size of unprocessed baitų šmotas.
+         * Will be 0 if the data has been preprocessed.
+         */
         std::size_t get_size();
-        Type        get_type();
+
+        Type get_type();
+        /**
+         * Get preprocessed data if a preprocessor was run.
+         * If no preprocessor was run, the object will have no value.
+         */
+        template <typename T> T &get_preprocessed() {
+            assert(std::holds_alternative<std::any>(this->data));
+
+            auto &any = std::get<std::any>(this->data);
+
+            assert(any.has_value());
+
+            return std::any_cast<T &>(any);
+        }
 
         class Loader {
         public:
-            static std::shared_ptr<Asset> load(Type type, const std::filesystem::path &path);
+            static std::shared_ptr<Asset> load(Type type, const std::filesystem::path &path, Preprocessor preprocessor);
         };
 
     private:
-        char       *data;
-        std::size_t size;
-        Type        type;
+        struct Bytes {
+            char       *data;
+            std::size_t size;
+        };
+
+        Type                          type;
+        std::variant<Bytes, std::any> data;
     };
 
     NLOHMANN_JSON_SERIALIZE_ENUM(Asset::Type, {
-                                                  {Asset::Type::INVALID, nullptr},
+                                                  {Asset::Type::DATA, nullptr},
                                                   {Asset::Type::DATA, "data"},
                                                   {Asset::Type::IMAGE, "image"},
                                                   {Asset::Type::SOUND, "sound"},
                                               })
+    NLOHMANN_JSON_SERIALIZE_ENUM(Asset::Preprocessor, {
+                                                          {Asset::Preprocessor::NONE, nullptr},
+                                                          {Asset::Preprocessor::JSON, "json"},
+                                                      })
 } // namespace milg
